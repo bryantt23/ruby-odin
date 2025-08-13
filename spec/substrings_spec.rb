851604e41d038cb23fb5c64f6658cae1239849fc
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-13 23:30:02 +0800

    add second test

--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe "Caesar Cipher" do
     expect("hello").to eq("hello")
   end
   it "basic test" do
-    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]   
-    expect( substrings("below", dictionary)).to eq({ "below" => 1, "low" => 1 })
+    dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+    expect(substrings("below", dictionary)).to eq({ "below" => 1, "low" => 1 })
+  end
+  it "can handle multiple words" do
+    dictionary = ["below", "down", "go", "going", "horn", "how", "howdy", "it", "i", "low", "own", "part", "partner", "sit"]
+    expect(substrings("Howdy partner, sit down! How's it going?", dictionary)).to eq(
+      { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+    )
   end
 end
