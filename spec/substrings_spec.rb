a838ba761b7edb35782472c1423e5028eabceae6
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-13 23:18:04 +0800

    first test

--- spec/substrings_spec.rb
+++ spec/substrings_spec.rb
@@ -1,8 +1,12 @@
 require "spec_helper"
-require_relative "../lib/caesar_cipher"
+require_relative "../lib/substrings"
 
 RSpec.describe "Caesar Cipher" do
   it "runs hello world test" do
     expect("hello").to eq("hello")
+  end
+  it "basic test" do
+    dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]   
+    expect( substrings("below", dictionary)).to eq({ "below" => 1, "low" => 1 })
   end
 end
