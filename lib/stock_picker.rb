67035769666268122089e3f86b5ac70d3a523301
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-14 00:09:36 +0800

    clean up

--- lib/stock_picker.rb
+++ lib/stock_picker.rb
@@ -1,20 +1,21 @@
 def stock_picker(prices)
   lowest_price = Float::INFINITY
-  max_profit = -Float::INFINITY
+  max_profit = 0
   lowest_price_index = -1
   highest_price_index = -1
   res = []
   prices.each_with_index do |price, index|
+    puts price
     if price < lowest_price
       lowest_price = price
       lowest_price_index = index
-    else
-      cur_profit = price - lowest_price
-      if cur_profit > max_profit
-        max_profit = cur_profit
-        highest_price_index = index
-        res = [lowest_price_index, highest_price_index]
-      end
+    end
+
+    cur_profit = price - lowest_price
+    if cur_profit > max_profit
+      max_profit = cur_profit
+      highest_price_index = index
+      res = [lowest_price_index, highest_price_index]
     end
   end
   res
