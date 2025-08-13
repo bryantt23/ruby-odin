2a7ae6676da8646339eca9a1f26af8e320d845b4
bryant tunbutr <bryantt23@users.noreply.github.com>
2025-08-14 01:06:35 +0800

    bubble sort

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,1 +1,18 @@
-
+def bubble_sort(arr)
+  n = arr.length
+  last_pos = n - 1
+  while last_pos > 0
+    i = 0
+    while i < last_pos
+      next_pos = i + 1
+      if arr[i] > arr[next_pos]
+        temp = arr[i]
+        arr[i] = arr[next_pos]
+        arr[next_pos] = temp
+      end
+      i += 1
+    end
+    last_pos -= 1
+  end
+  arr
+end
